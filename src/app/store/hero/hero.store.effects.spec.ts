09eca9ac50db536d1481051ad2cbb841a1c7418c
BJ Anderson <andersonb79@gmail.com>
2018-02-25 20:25:58 -0500

    added more unit tests

--- src/app/store/hero/hero.store.effects.spec.ts
+++ src/app/store/hero/hero.store.effects.spec.ts
@@ -5,7 +5,7 @@ import { HeroEffects } from './hero.store.effects';
 describe('HeroEffects', function () {
   let effects: HeroEffects;
   const actions$: any = empty();
-  const heroService: any = { get: empty() };
+  const heroService: any = { getAll: () => empty() };
 
   describe('constructor', function () {
     beforeEach(() => {
@@ -14,6 +14,29 @@ describe('HeroEffects', function () {
 
     it('has a function named ', function () {
       expect(effects).toBeDefined();
+    });
+  });
+
+  describe('loadHeroes()', function () {
+    beforeEach(() => {
+      effects = new HeroEffects(actions$, heroService);
+    });
+
+    it('has a function named loadHeroes', function () {
+      expect(typeof effects.loadHeroes).toEqual('function');
+    });
+
+    it('calls heroService.getAll()', function () {
+      spyOn(heroService, 'getAll').and.callThrough();
+      effects.loadHeroes();
+      expect(heroService.getAll).toHaveBeenCalled();
+    });
+
+    it('returns an observable', function () {
+      const observable = effects.loadHeroes();
+      const expected: any = 'function';
+      const result: any = typeof observable.subscribe;
+      expect(result).toEqual(expected);
     });
   });
 });
--- src/app/store/villain/villain.store.effects.spec.ts
+++ src/app/store/villain/villain.store.effects.spec.ts
@@ -5,7 +5,7 @@ import { VillainEffects } from './villain.store.effects';
 describe('VillainEffects', function () {
   let effects: VillainEffects;
   const actions$: any = empty();
-  const villainService: any = { get: empty() };
+  const villainService: any = { getAll: () => empty() };
 
   describe('constructor', function () {
     beforeEach(() => {
@@ -14,6 +14,29 @@ describe('VillainEffects', function () {
 
     it('has a function named ', function () {
       expect(effects).toBeDefined();
+    });
+  });
+
+  describe('loadVillains()', function () {
+    beforeEach(() => {
+      effects = new VillainEffects(actions$, villainService);
+    });
+
+    it('has a function named loadVillains', function () {
+      expect(typeof effects.loadVillains).toEqual('function');
+    });
+
+    it('calls villainService.getAll()', function () {
+      spyOn(villainService, 'getAll').and.callThrough();
+      effects.loadVillains();
+      expect(villainService.getAll).toHaveBeenCalled();
+    });
+
+    it('returns an observable', function () {
+      const observable = effects.loadVillains();
+      const expected: any = 'function';
+      const result: any = typeof observable.subscribe;
+      expect(result).toEqual(expected);
     });
   });
 });
